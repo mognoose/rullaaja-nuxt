570d4f3255dc8d08d8c8002f01c837f984c926be
sapatti <mognoose@gmail.com>
2023-07-26 13:57:41 +0300

    fix cors5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  runtimeConfig: {
-    APPID: '1074965099007197235',
-    PUBLIC_KEY: '0fa564d7037045cf70a1279f7e7f193f386997ca8cbcd0559399de17efd99cfd',
-    TOKEN: 'MTA3NDk2NTA5OTAwNzE5NzIzNQ.GJf0pv.-bPCpFrYqHyqxpkUgzpEZ2bvmIF9SM46n55Rfo',
-    CHANNELID: '479199736776228865',
-    INVITE_LINK: '"https://discord.com/oauth2/authorize?client_id=1074965099007197235&scope=bot&permissions=380104636416"',
-    public: {
-      
-    }
-  },
   srcDir: 'src',
   devServer: {
     port: Number(process.env.NUXT_PORT ?? 8080),
@@ -17,5 +7,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  routeRules: {
+    'api/**': {cors: true, headers: { 'access-control-allow-methods': 'POST' }}
   },
 })
